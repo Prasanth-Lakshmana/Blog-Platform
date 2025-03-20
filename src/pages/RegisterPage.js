dab1fecd84eafe9c5bcfbdab7606143bcc9e3900
Prasanth-Lakshmana <146560372+Prasanth-Lakshmana@users.noreply.github.com>
2025-03-20 12:12:39 +0530

    Commit

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -12,7 +12,7 @@ export default function RegisterPage() {
   async function register(ev) {
     try {
       ev.preventDefault();
-      const response = await fetch("https://blog-backend-qex9.onrender.com/register", {
+      const response = await fetch("https://blog-backend-qex9.onrender.com/api/register", {
         method: "POST",
         body: JSON.stringify({ username, password }),
         headers: { "Content-Type": "application/json" },
